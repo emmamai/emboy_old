b6448174c6137dfca2aad63ae1bde5c47d9236f4
em <em@emmamai.xyz>
2016-02-27 14:31:09 -0500

    MemMap::Poke functionality added, some opcodes implemented

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ impl Gameboy {
 		}
 	}
 	fn run_clock_cycle(&mut self) {
-
+		self.cpu.run_clock_cycle(&mut self.mmap);
 	}
 	fn hard_reset(&mut self) {
 		self.cpu.reg_a = 0x01;
@@ -65,11 +65,26 @@ impl MemMap {
 			0xFF00 ... 0xFF7F => { println!("Write to unimplemented IO port address {}", addr); 0 },
 			0xFF80 ... 0xFFFE => self.hram[(addr-0xFF80) as usize],
 			0xFFFF => self.reg_interrupt_enable,
-			_ => panic!("MemMap::addr No mapping for address {}", addr),
+			_ => panic!("MemMap::addr No mapping for address 0x{0:04X}", addr),
 		}
 	}
 	fn poke(&mut self, addr: u16, val: u8) {
-		panic!("MemMap::poke Not implemented yet!")
+		match addr {
+			0x0000 ... 0x3fff => { self.cartridge.poke(addr, val); },
+			0x4000 ... 0x7fff => { self.cartridge.poke(addr, val); },
+			0x8000 ... 0x9FFF => { self.vram[(addr-0x8000) as usize] = val; },
+			0xA000 ... 0xBFFF => { self.cartridge.poke(addr, val); },
+			0xC000 ... 0xCFFF => { self.ram[(addr-0xC000) as usize] = val; },
+			0xD000 ... 0xDFFF => { self.ram[(addr-0xC000) as usize] = val; },
+			0xE000 ... 0xEFFF => { self.ram[(addr-0xE000) as usize] = val; },
+			0xF000 ... 0xFDFF => { self.ram[(addr-0xE000) as usize] = val; },
+			0xFE00 ... 0xFE9F => { self.oam[(addr-0xFE00) as usize] = val; },
+			0xFEA0 ... 0xFEFF => { println!("Read from unusable address space 0xFEA0-0xFEFF"); },
+			0xFF00 ... 0xFF7F => { println!("Write to unimplemented IO port address {}", addr); },
+			0xFF80 ... 0xFFFE => { self.hram[(addr-0xFF80) as usize]; },
+			0xFFFF => { self.reg_interrupt_enable = val; },
+			_ => panic!("MemMap::addr No mapping for address 0x{0:04X}", addr),
+		};
 	}
 }
 
@@ -126,14 +141,68 @@ impl GbCpu {
 	fn run_instruction(&mut self, mmap: &mut MemMap) {
 		let instruction = mmap.peek(self.reg_pc);
 
-		print!("GbCpu: [0x{0:x}] 0x{1:x} ", self.reg_pc, instruction);
+		print!("[0x{0:04X}] 0x{1:02X} 	", self.reg_pc, instruction);
 		match instruction {
-			0x00 => { //NOP
-						self.ins_cycles = 0;
-						self.ins_max_cycles = 4;
-						self.reg_pc = self.reg_pc + 1;
-						println!("NOP");
-					}
+			0x00 => {
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 4;
+				self.reg_pc = self.reg_pc + 1;
+				println!("NOP");
+			}
+			0x06 => {
+				self.reg_b = mmap.peek(self.reg_pc + 2);
+				
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 8;
+				self.reg_pc = self.reg_pc + 2;
+				println!("LD B, 0x{0:02X}", self.reg_h);
+			}
+			0x0E => {
+				self.reg_c = mmap.peek(self.reg_pc + 2);
+				
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 8;
+				self.reg_pc = self.reg_pc + 2;
+				println!("LD C, 0x{0:02X}", self.reg_h);
+			}
+			0x21 => {
+				self.reg_h = mmap.peek(self.reg_pc + 2);
+				self.reg_l = mmap.peek(self.reg_pc + 1);
+				
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 12;
+				self.reg_pc = self.reg_pc + 3;
+				println!("LD HL, 0x{0:02X}{1:02X}", self.reg_h, self.reg_l);
+			}
+			0x32 => {
+				let address = ((self.reg_h as u16)<< 8 ) + (self.reg_l as u16);
+				mmap.poke(address, self.reg_a);
+
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 4;
+				self.reg_pc = self.reg_pc + 1;
+				println!("LD (HL), A : (0x{0:04X}) <- 0x{1:02X}", address, self.reg_a);
+			}
+			0xC3 => {
+				let new_address_high = mmap.peek(self.reg_pc + 2) as u16;
+				let new_address_low = mmap.peek(self.reg_pc + 1) as u16;
+				let new_address = ( new_address_high << 8 ) + new_address_low;
+				
+
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 16;
+				self.reg_pc = new_address;
+				println!("JP 0x{0:04X}", new_address);
+			}
+			0xAF => {
+				self.reg_a = 0;
+				self.reg_f = self.reg_f & 0x7F;
+
+				self.ins_cycles = 0;
+				self.ins_max_cycles = 4;
+				self.reg_pc = self.reg_pc + 1;
+				println!("XOR a");
+			}
 			_ => println!("Unimplemented instruction"),
 		}
 	}
@@ -185,7 +254,7 @@ fn main() {
     
 
   	gb.hard_reset();
-    for i in 0..16 {
+    for i in 0..96{
     	gb.cpu.run_clock_cycle(&mut gb.mmap);
     }
 }
